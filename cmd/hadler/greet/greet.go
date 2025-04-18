66616ce4b4e15aa50ea4db6b137c732223cc3dc8
Alisher Ghulomzoda <a.gulomzoda7@gmail.com>
2025-04-18 09:09:49 +0500

    add update

--- cmd/hadler/greet/greet.go
+++
@@ -1,8 +0,0 @@
-package greet
-
-import "github.com/gofiber/fiber/v3"
-
-func (h *handler) Greet(c fiber.Ctx) error {
-	// Send a string response to the client
-	return c.SendString("Salom aleykum 👋!")
-}
--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	greetHandlerModul "farrukh_golang/cmd/hadler/greet"
 	"farrukh_golang/cmd/hadler/math"
 	"farrukh_golang/cmd/hadler/my_balance"
-	greetServiceModul "farrukh_golang/internal/greet"
 	mathSrv "farrukh_golang/internal/math"
 	my_balance2 "farrukh_golang/internal/my_balance"
 	"github.com/gofiber/fiber/v3"
@@ -14,16 +12,6 @@ import (
 func main() {
 	// Initialize a new Fiber app
 	app := fiber.New()
-
-	greetService := greetServiceModul.New()
-	greetHandler := greetHandlerModul.New(greetService)
-
-	// Define a route for the GET method on the root path '/'
-	app.Get("/assalom", greetHandler.Greet)
-	app.Get("/name/:name/gender/:gender", greetHandler.Meet)
-
-	//balanceService := service.NewBalanceService()
-	//balanceHandler := handler.NewBalanceHandler(balanceService)
 
 	mybalanceService := my_balance2.New()
 	myBalansHandler := my_balance.New(mybalanceService)
